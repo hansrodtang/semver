f096122e5b6ab162bd8edd646c41200bb54cd35b
Hans Rødtang <hansrodtang@gmail.com>
2014-10-26 02:48:02 +0100

    Refactored JSON interfaces.

--- interfaces.go
+++ interfaces.go
@@ -8,17 +8,10 @@ import (
 func (v *Version) UnmarshalJSON(b []byte) error {
 	input := strings.Trim(string(b), "\"")
 	ver, err := New(input)
-	if err != nil {
-		return err
-	}
 	*v = *ver
-	return nil
+	return err
 }
 
 func (p *Version) MarshalJSON() ([]byte, error) {
-	output, err := json.Marshal(p.String())
-	if err != nil {
-		return nil, err
-	}
-	return output, nil
+	return json.Marshal(p.String())
 }
