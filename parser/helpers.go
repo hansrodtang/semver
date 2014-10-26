fdd42b3f02aed89ffef16c7e7ee5aedc22ef86f4
Hans Rødtang <hansrodtang@gmail.com>
2014-10-26 02:48:43 +0200

    Removed unfinished converters.

--- parser/helpers.go
+++ parser/helpers.go
@@ -24,15 +24,3 @@ func lte(main, other *semver.Version) bool {
 func eq(main, other *semver.Version) bool {
 	return main.Compare(other) == 0
 }
-
-func rng2cmp(main, other *semver.Version) satisfactionMap {
-	return satisfactionMap{main: gt, other: lt}
-}
-
-func cr2cmp(*semver.Version) satisfactionMap {
-	return nil
-}
-
-func tld2cmp(*semver.Version) satisfactionMap {
-	return nil
-}
