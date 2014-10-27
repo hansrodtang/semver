6a840a521b8633e1a8691aae844b269b248a7a4d
Hans Rødtang <hansrodtang@gmail.com>
2014-10-27 13:33:31 +0100

    Added more tests for new parse rules.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -19,10 +19,42 @@ var parsables = map[string][]test{
 		{false, semver.Build(1, 2, 8)},
 		{false, semver.Build(2, 0, 0)},
 	},
-	//"1.2 <1.2.9 || >2.0.0": {
-	//	{false, semver.Build(1,2,10)},
-	//  {true, semver.Build(1,2,8)}
-	//}
+	"1.2 <1.2.9 || >2.0.0": {
+		{false, semver.Build(1, 2, 10)},
+		{false, semver.Build(1, 5, 1)},
+		{true, semver.Build(1, 2, 8)},
+		{true, semver.Build(1, 2, 7)},
+	},
+	"* || >2.0.0": {
+		{true, semver.Build(1, 0, 10)},
+		{true, semver.Build(100, 5, 1)},
+		{true, semver.Build(1, 100, 8)},
+		{true, semver.Build(1, 2, 100)},
+	},
+	"* >2.0.0": {
+		{false, semver.Build(1, 0, 10)},
+		{true, semver.Build(100, 5, 1)},
+		{false, semver.Build(1, 100, 8)},
+		{false, semver.Build(1, 2, 100)},
+	},
+	"1.0.0 - 2.0.0": {
+		{false, semver.Build(0, 0, 10)},
+		{false, semver.Build(3, 5, 1)},
+		{true, semver.Build(1, 1, 5)},
+		{true, semver.Build(1, 9, 7)},
+	},
+	"~1.2.3": {
+		{false, semver.Build(1, 3, 2)},
+		{false, semver.Build(1, 2, 2)},
+		{true, semver.Build(1, 2, 5)},
+		{true, semver.Build(1, 2, 9)},
+	},
+	"~1.2": {
+		{false, semver.Build(1, 3, 2)},
+		{false, semver.Build(1, 1, 9)},
+		{true, semver.Build(1, 2, 3)},
+		{true, semver.Build(1, 2, 9)},
+	},
 }
 
 func TestParser(t *testing.T) {
