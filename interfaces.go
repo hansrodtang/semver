6ab8dad86c227508080b3c55ac8a68943e3c5320
Hans Rødtang <hansrodtang@gmail.com>
2014-10-27 04:14:06 +0100

    Added sortable Version slice.

--- interfaces.go
+++ interfaces.go
@@ -15,3 +15,17 @@ func (v *Version) UnmarshalJSON(b []byte) error {
 func (p *Version) MarshalJSON() ([]byte, error) {
 	return json.Marshal(p.String())
 }
+
+type Versions []*Version
+
+func (v Versions) Len() int {
+	return len(v)
+}
+
+func (v Versions) Swap(i, j int) {
+	v[i], v[j] = v[j], v[i]
+}
+
+func (v Versions) Less(i, j int) bool {
+	return v[i].Compare(v[j]) < 0
+}
--- interfaces_test.go
+++ interfaces_test.go
@@ -2,6 +2,7 @@ package semver
 
 import (
 	"encoding/json"
+	"sort"
 	"testing"
 )
 
@@ -36,4 +37,50 @@ func TestMarshal(t *testing.T) {
 	if string(result) != string(jsonString) {
 		t.Errorf("MarshalJSON(%q) => %v, want %v", ver, string(result), string(jsonString))
 	}
+}
+
+var unsortedVersions = []string{
+	"1.2.3-alpha.2+123.456",
+	"1.2.3-alpha.1",
+	"1.4.3+123.456",
+	"1.3.3-alpha.b-eta+123.b-uild",
+	"1.2.3+123.b-uild",
+	"1.2.3-alpha.b-eta",
+	"0.0.1-alpha.preview+123.456",
+}
+
+var sortedVersions = []string{
+	"0.0.1-alpha.preview+123.456",
+	"1.2.3-alpha.1",
+	"1.2.3-alpha.2+123.456",
+	"1.2.3-alpha.b-eta",
+	"1.2.3+123.b-uild",
+	"1.3.3-alpha.b-eta+123.b-uild",
+	"1.4.3+123.456",
+}
+
+func TestSorter(t *testing.T) {
+	var unsortedlist Versions
+	for _, v := range unsortedVersions {
+		ver, _ := New(v)
+		unsortedlist = append(unsortedlist, ver)
+	}
+
+	sort.Sort(unsortedlist)
+
+	var sortedlist Versions
+	for _, v := range sortedVersions {
+		ver, _ := New(v)
+		sortedlist = append(sortedlist, ver)
+	}
+
+	for i, r := range unsortedlist {
+		result := r.String()
+		expected := sortedlist[i].String()
+
+		if result != expected {
+			t.Errorf("sort.Sort() => %q, want %v", result, expected)
+		}
+	}
+
 }
